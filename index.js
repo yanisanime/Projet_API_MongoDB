cb2371df67e6fed66cb27d40f55d2e852aaef704
yanis <meriem.yanis05@gmail.com>
2025-03-24 15:59:55 +0100

    setup du projet

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  //pour afficher l'adresse du serveur
+    console.log(`http://localhost:${port}`)
+})
